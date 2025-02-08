b7fe01c7af90d4f7c9c4cb7c4426f8f01b5e5155
Matheus <matheusalmeidass14@gmail.com>
2025-02-08 11:46:11 -0300

    feat: basic func created: criaListaDePessoas, criaPessoa, inserePessoaNaListaDeNovos

--- include/pessoa.h
+++ include/pessoa.h
@@ -5,4 +5,25 @@
 #ifndef TRABALHO_FINAL_ESTRUTURA_DE_DADOS_PESSOA_H
 #define TRABALHO_FINAL_ESTRUTURA_DE_DADOS_PESSOA_H
 
+#include <time.h>
+
+typedef struct pessoa {
+    int codigo;
+    char nome[50];
+    int telefone;
+    struct tm dataDeNascimento;
+    char endereco[50];
+    struct pessoa * prox;
+    struct pessoa * ant;
+}Pessoa;
+
+typedef struct listaDePessoas {
+    int quantidadeTotalDePessoas;
+    struct pessoa * cabeca;
+}ListaDePessoas;
+
+ListaDePessoas * criaListaDePessoas();
+Pessoa * criaPessoa(int codigo, char * nome, int telefone, struct tm dataDeNascimento, char * endereco);
+ListaDePessoas * inserePessoaNaListaDeNovos(ListaDePessoas ** listaDePessoas, Pessoa * novaPessoa);
+
 #endif //TRABALHO_FINAL_ESTRUTURA_DE_DADOS_PESSOA_H
--- src/pessoa.c
+++ src/pessoa.c
@@ -1,3 +1,61 @@
 //
 // Created by Casa on 06/02/2025.
 //
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#include "..\include\pessoa.h"
+
+ListaDePessoas * criaListaDePessoas() {
+    ListaDePessoas * listaDePessoas = (ListaDePessoas *) malloc(sizeof(typeof(ListaDePessoas)));
+    if(listaDePessoas == NULL) {
+        return NULL;
+    }
+
+    listaDePessoas->cabeca = NULL;
+    listaDePessoas->quantidadeTotalDePessoas = 0;
+
+    return listaDePessoas;
+}
+
+Pessoa * criaPessoa(int codigo, char * nome, int telefone, struct tm dataDeNascimento, char * endereco) {
+    Pessoa * novaPessoa = (Pessoa *) malloc(sizeof(typeof(Pessoa)));
+    if(novaPessoa == NULL) {
+        return NULL;
+    }
+
+    novaPessoa->codigo = codigo;
+    strcpy(novaPessoa->nome, nome);
+    novaPessoa->telefone = telefone;
+    novaPessoa->dataDeNascimento = dataDeNascimento;
+    strcpy(novaPessoa->endereco, endereco);
+
+    novaPessoa->prox = NULL;
+    novaPessoa->ant = NULL;
+
+    return novaPessoa;
+}
+
+ListaDePessoas * inserePessoaNaListaDeNovos(ListaDePessoas ** listaDePessoas, Pessoa * novaPessoa) {
+    if(novaPessoa == NULL) {
+        return NULL;
+    }
+
+    if((*listaDePessoas)->cabeca != NULL) {
+        novaPessoa->prox = (*listaDePessoas)->cabeca;
+        (*listaDePessoas)->cabeca->ant = novaPessoa;
+    }
+
+    (*listaDePessoas)->cabeca = novaPessoa;
+    (*listaDePessoas)->quantidadeTotalDePessoas++;
+
+    return *listaDePessoas;
+}
+
+//inserir pessoa no bin
+// deletar pessoa
+// exibir pessoa
+//extrair pessoa no bin
