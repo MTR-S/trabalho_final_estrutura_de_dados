d59f0c1c86845abde1612499eded3eae128f82a2
Matheus <matheusalmeidass14@gmail.com>
2025-02-10 11:09:56 -0300

    feat: testes das funcoes

---
+++ include/menu.h
@@ -0,0 +1,12 @@
+//
+// Created by Casa on 09/02/2025.
+//
+
+#ifndef TRABALHO_FINAL_ESTRUTURA_DE_DADOS_MENU_H
+#define TRABALHO_FINAL_ESTRUTURA_DE_DADOS_MENU_H
+
+#include "pessoa.h"
+
+void exibirPessoa(Pessoa pessoa);
+
+#endif //TRABALHO_FINAL_ESTRUTURA_DE_DADOS_MENU_H
---
+++ src/menu.c
@@ -0,0 +1,18 @@
+//
+// Created by Casa on 09/02/2025.
+//
+
+#include <stdio.h>
+
+#include "../include/menu.h"
+#include "../include/pessoa.h"
+
+void exibirPessoa(Pessoa pessoa) {
+    printf("*******************************\n");
+    printf("Codigo: %d\n", pessoa.codigo);
+    printf("Nome: %s\n", pessoa.nome);
+    printf("Telefone: %d\n", pessoa.telefone);
+    printf("Data Formatada: %s\n", pessoa.dataFormatada);
+    printf("Endereco: %s\n", pessoa.endereco);
+    printf("*******************************\n");
+}
